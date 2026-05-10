416b15e24f93519c9675a54c221b55651abcb14d
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-05-10 00:44:44 -0300

    fix: importing the env module at database module

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -4,8 +4,10 @@ import { UserRepository } from "../users/repositories/user.repository";
 import { PrismaService } from "./prisma/prisma.service";
 import { PrismaNoteRepository } from "./prisma/repositories/prisma-note.repository";
 import { PrismaUserRepository } from "./prisma/repositories/prisma-user.repository";
+import { EnvModule } from "../env/env.module";
 
 @Module({
+    imports: [EnvModule],
     providers: [
         PrismaService,
         {
