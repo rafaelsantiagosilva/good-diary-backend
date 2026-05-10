add3482e00147c31b38703e4e08288cec640067d
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-05-10 01:21:33 -0300

    test: configuring the e2e tests

---
+++ test/utils/prisma-reset.ts
@@ -0,0 +1,19 @@
+import { PrismaService } from "src/modules/database/prisma/prisma.service";
+
+export async function resetDatabase(prisma: PrismaService) {
+    const tableNames = await prisma.client.$queryRaw<
+        Array<{ tablename: string }>
+    >`SELECT tablename FROM pg_tables WHERE schamename='public'`;
+
+    const tables = tableNames
+        .map(({ tablename }) => tablename)
+        .filter((name) => name !== "_prisma_migrations")
+        .map((name) => `"public"."${name}"`)
+        .join(", ");
+
+    try {
+        await prisma.client.$executeRawUnsafe(`TRUNCATE TABLE ${tables} CASCADE;`);
+    } catch (error) {
+        console.error("Error when cleaning the test database!", error);
+    }
+}
