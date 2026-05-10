f293a64db500f4e76380e43b62937be4b1a3259c
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-05-09 23:29:06 -0300

    feat: creating the EnvService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from "@nestjs/config";
+import { envSchema } from './modules/env/env';
+import { EnvService } from './modules/env/env.service';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ".env"
+      validate: env => envSchema.parse(env)
     })
   ],
+  providers: [EnvService]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/modules/env/env.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { z } from "zod";
+
+export const envSchema = z.object({
+    PORT: z.coerce.number().default(3000),
+    JWT_KEY: z.string(),
+    DB_PASS: z.string(),
+    DB_USER: z.string(),
+    DB_NAME: z.string(),
+    DATABASE_URL: z.url()
+});
+
+export type Env = z.infer<typeof envSchema>;
