633051c0fd6f0ed7935792c6b98d1e1e2f6622e9
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-05-09 23:21:44 -0300

    feat: creating the database module

---
+++ src/modules/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from "@nestjs/common";
+import { NoteRepository } from "../notes/repositories/note.repository";
+import { UserRepository } from "../users/repositories/user.repository";
+import { PrismaService } from "./prisma/prisma.service";
+import { PrismaNoteRepository } from "./prisma/repositories/prisma-note.mapper";
+import { PrismaUserRepository } from "./prisma/repositories/prisma-user.repository";
+
+@Module({
+    providers: [
+        PrismaService,
+        {
+            provide: UserRepository,
+            useClass: PrismaUserRepository
+        },
+        {
+            provide: NoteRepository,
+            useClass: PrismaNoteRepository
+        }
+    ],
+    exports: [
+        PrismaService,
+        UserRepository,
+        NoteRepository
+    ]
+})
+export class DatabaseModule { }
